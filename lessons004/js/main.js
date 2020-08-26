ff1ec1d6dda5d7ce87ee13255b61001f304405cf
Neksov <n2v@list.ru>
2020-08-26 13:29:24 +0300

    new

--- lessons004/js/main.js
+++ lessons004/js/main.js
@@ -20,3 +20,36 @@ console.log(person.name);
 
 let arr = ['plus.png', 'orange.png', 'first.png'];
 console.log(arr[1]);
+
+
+//Динамическая типизация
+
+//-to String
+//1)
+console.log(typeof (String(65)));
+//2)
+console.log(typeof (String("ww" + "223")));
+//На практике
+
+
+//-to Number
+//1)
+console.log(typeof (Number('65')));
+//2)
+console.log(typeof (+'5'));
+//3
+console.log(typeof (parseInt('15px', 10)));
+//На практике
+//let ans = +prompt("hello?", '');
+
+let switcher = null;
+if (switcher) {
+  console.log('Working...');
+}
+switcher = 1;
+if (switcher) {
+  console.log('Working...');
+}
+
+//3
+console.log(typeof (Boolean('65')));
