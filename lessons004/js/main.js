5d1c354e08ac84d37bc003b86211aed23a34bda8
Neksov <n2v@list.ru>
2020-08-26 12:57:39 +0300

    new lessons

---
+++ lessons004/js/main.js
@@ -0,0 +1,22 @@
+var number = 5;
+var string = "hello";
+var syn = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4 / 0);
+console.log('string' * 9);
+let somesing;
+console.log(somesing);
+
+let person = {
+  name: 'Jone',
+  age: 25,
+  isMarried: false
+};
+console.log(person.name);
+
+let arr = ['plus.png', 'orange.png', 'first.png'];
+console.log(arr[1]);
