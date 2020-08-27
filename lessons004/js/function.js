cf8d4b2c60ed0ecfc9e784aaab80961e2c9e8945
Вадим Некрасов <n2v@list.ru>
2020-08-27 13:34:47 +0300

    rewrite js file fuction

---
+++ lessons004/js/function.js
@@ -0,0 +1,35 @@
+// let num = 20;//глобальная 
+// function showFirstMessage(text) {
+//   alert(text);
+//   //let num = 10;//локальная 
+//   console.log(num);
+// }
+// showFirstMessage("HELLO WORLD");
+// console.log(num);
+
+
+// let calc = function (a, b) {
+//   return (a + b);
+// }
+// let calc = (a, b) => a + b;
+// console.log(calc(5, 6));
+// console.log(calc(1, 3));
+
+// function retVar() {
+//   let num = 50;
+//   return num;
+// }
+
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.2px";
+//console.log(Math.round(twelve));
+console.log(parseInt(twelve));// перевод в целую чиловую систему исчисления 
+console.log(parseFloat(twelve));// перевод в дробную чиловую систему исчисления 
